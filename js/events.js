93f6645cbc0a84aae1beb974a184db11489a713a
thunguy@me.com <thunguy@me.com>
2018-06-07 05:09:07 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -15,8 +15,20 @@ function frameIt() {
 
 function pressIt() {
   $("form").on('keydown', function() {
-    if ($())
+    if ($('input:first').val() === "G") {
+      alert("G was pressed");
+      return;
+    }
   })
+}
+
+function submitIt() {
+  $("form").on('submit', function() {
+    alert("Your form is going to be submitted now.");
+    return;
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
