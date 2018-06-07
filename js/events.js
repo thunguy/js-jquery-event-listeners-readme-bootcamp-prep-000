4758f76650fed6deb5d39fe391d586ffe58a40d1
thunguy@me.com <thunguy@me.com>
2018-06-07 05:42:59 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+  alert('Hey!');
+  return;
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+  return $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert("G was pressed");
+      return;
+    }
+  })
+}
+
+function submitIt() {
+  $("form").on('submit', function() {
+    alert("Your form is going to be submitted now.");
+    return;
+  })
+}
+
 $(document).ready(function(){
-
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
