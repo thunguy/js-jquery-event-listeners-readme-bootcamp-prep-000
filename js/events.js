db8eec3dba7f90c1ec90a1c5fa0553d064c3bac1
thunguy@me.com <thunguy@me.com>
2018-06-07 04:53:17 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,14 @@
 //define functions here
 
+$('p').on('click', function getIt() {
+  alert('Hey!');
+  return;
+})
+
+$('.tasty').on('load', function frameIt() {
+  
+}
+
 $(document).ready(function(){
 
 // call functions here
